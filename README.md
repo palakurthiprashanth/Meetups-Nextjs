d123899e526983ac403350efed958c14fb58dd44
palakurthiprashanth <palakurthi.prashanth1@gmail.com>
2021-08-22 08:31:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-qhzpl1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-qhzpl1)
---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main className={styles.main}>Main</main>
+
+      <footer className={styles.footer}>Footer</footer>
+    </div>
+  );
+}
